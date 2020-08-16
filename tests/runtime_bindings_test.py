3c7b33233bd3ef50829aa13f31748f43385deff4
panda1920 <15102478+panda1920@users.noreply.github.com>
2020-08-16 09:12:05 +0900

    - Finished writing tests for runtime_binding

--- tests/runtime_bindings_test.py
+++ tests/runtime_bindings_test.py
@@ -11,7 +11,8 @@ from twitter_image_dl.settings import Settings
 import twitter_image_dl.exceptions as exceptions
 import twitter_image_dl.setting_strings as strings
 
-PROJECT_DIR = Path(__file__).resolve().parents[0]
+PROJECT_DIR = Path(__file__).resolve().parents[1]
+DOWNLOAD_DIR = PROJECT_DIR / 'testdata' / 'download'
 
 @pytest.fixture(scope='function')
 def mocks():
@@ -37,7 +38,7 @@ def mocks():
     :
         mock_retriever.return_value = create_autospec(TweetsRetriever_TwitterAPI)
         mock_history.return_value = create_autospec(DownloadHistory)
-        mock_settings.return_value = create_autospec(Settings)
+        implement_mock_settings(mock_settings)
 
         yield dict(
             readuserlist=mock_readuserlist,
@@ -46,6 +47,27 @@ def mocks():
             settings=mock_settings,
             downloadmedia=mock_downloadmedia
         )
+
+@pytest.fixture(scope='function', autouse=True)
+def clear_download_dir():
+    yield
+    shutil.rmtree(DOWNLOAD_DIR)
+    DOWNLOAD_DIR.mkdir()
+
+def implement_mock_settings(mock_settings):
+    mock_settings.return_value = create_autospec(Settings)
+
+    mock_settings.return_value.get.return_value = {
+        strings.APP_SECTION: {
+            strings.SAVE_LOCATION: DOWNLOAD_DIR
+        },
+        strings.API_SECTION: {
+            strings.ACCESS_TOKEN: 'some_value',
+            strings.ACCESS_SECRET: 'some_value',
+            strings.CONSUMER_KEY: 'some_value',
+            strings.CONSUMER_SECRET: 'some_value',
+        }
+    }
 
 class TestValidations:
     def test_raiseErrorWhenNoAPIOptionsAreFoundInSettings(self, tmp_path, mocks):
@@ -79,3 +101,39 @@ class TestValidations:
 
         with pytest.raises(exceptions.SaveLocationNotExist) as e:
             bindings = RuntimeBindings(tmp_path)
+
+class TestInstantiation:
+    def test_pathPassedToRuntimeBindingsIsGivenToSettings(self, tmp_path, mocks):
+        bindings = RuntimeBindings(tmp_path)
+
+        assert len(mocks['settings'].call_args_list) == 1
+        path, *_ = mocks['settings'].call_args_list[0][0]
+        assert path == tmp_path / 'settings.conf'
+
+    def test_pathPassedToReadUserListIsSaveLocationInSetting(self, tmp_path, mocks):
+        bindings = RuntimeBindings(tmp_path)
+        settings = mocks['settings'](DOWNLOAD_DIR).get()
+        save_location = settings[strings.APP_SECTION][strings.SAVE_LOCATION]
+
+        assert len(mocks['readuserlist'].call_args_list) == 1
+        path, *_ = mocks['readuserlist'].call_args_list[0][0]
+        assert path == save_location / 'users.txt'
+
+    def test_pathPassedToHistoryIsSaveLocationInSetting(self, tmp_path, mocks):
+        bindings = RuntimeBindings(tmp_path)
+        settings = mocks['settings'](DOWNLOAD_DIR).get()
+        save_location = settings[strings.APP_SECTION][strings.SAVE_LOCATION]
+
+        assert len(mocks['history'].call_args_list) == 1
+        path, *_ = mocks['history'].call_args_list[0][0]
+        assert path == save_location / 'history.json'
+
+    def test_instantiatedSettingAndHistoryIsPassedToRetriever(self, tmp_path, mocks):
+        bindings = RuntimeBindings(tmp_path)
+        settings = mocks['settings'].return_value
+        history = mocks['history'].return_value
+
+        assert len(mocks['retriever'].call_args_list) == 1
+        passed_history, passed_settings, *_ = mocks['retriever'].call_args_list[0][0]
+        assert passed_history == history
+        assert passed_settings == settings
